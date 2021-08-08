a36055f7e0fc923d5b81650dbc0303f554e2da59
spaceled <spaceled@gmail.com>
2021-08-08 11:35:42 +0600

    --

---
+++ null-vs-undefined.js
@@ -0,0 +1,21 @@
+// Undefined Value
+// ..........................
+let pakhi;
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return
+}
+
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+
+const premik = {name: 'smart dude', phone: 456789};
+console.log(premik.gf);
+
+let fun = undefined;
+let ages = [2, 5, 9];
+console.log(ages[11])
+
+// Null Value
+// ...................
