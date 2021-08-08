6a96a7b7543f33f357a24b60f6a9ce3414418eed
spaceled <spaceled@gmail.com>
2021-08-08 11:17:05 +0600

    True vs False

---
+++ true-false.js
@@ -0,0 +1,18 @@
+// Falsy Value
+// 0
+// const name = '';
+// undefined
+// null
+// NaN
+// false
+
+// Truthy Value
+// '0', ' ', 'false', []
+let fname = [];
+console.log(fname)
+if(fname) {
+    console.log('Condition is True');
+}
+else {
+    console.log('condition is false')
+}
